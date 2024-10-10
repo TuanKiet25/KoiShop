30635c3ed0e3162d65d8b875a9ac4ba8c8d8c271
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-10-10 14:53:44 +0700

    reset

--- demo/src/main/java/com/example/demo/repository/VarietyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Entity.Variety;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VarietyRepository extends JpaRepository<Variety, Long> {
-    Variety findVarietiesById(Long id);
-    Variety findVarietiesByName(String name);
-}
