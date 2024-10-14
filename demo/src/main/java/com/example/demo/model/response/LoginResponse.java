d6686d10060588a3145525e862f3837b71ca9f3e
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-10-14 12:37:31 +0700

    clear

--- demo/src/main/java/com/example/demo/model/response/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.response;
-
-import com.example.demo.Entity.Account;
-import lombok.Data;
-
-@Data
-public class LoginResponse extends Account {
-    String token;
-}
--- demo/src/main/java/com/example/demo/repository/KoiPackRespository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Entity.KoiPack;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KoiPackRespository extends JpaRepository<KoiPack, Long>
-{
-    KoiPack findByKoiPackName(String name);
-}
