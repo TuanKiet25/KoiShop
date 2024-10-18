9070fa0db064a2662974e78898adde9925ead287
Larry <90505134+soloplayer345@users.noreply.github.com>
2024-10-18 19:02:52 +0700

    fix koi add repo and service

---
+++ src/main/java/com/example/demo/Service/ConsignmentService.java
@@ -0,0 +1,14 @@
+package com.example.demo.Service;
+
+import com.example.demo.repository.ConsignmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsignmentService
+{
+    @Autowired
+    private ConsignmentRepository consignmentRepository;
+
+    //
+}
